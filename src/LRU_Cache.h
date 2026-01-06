bd5e3a54d281c10e686f66276e523df0de8839d6
Assiiinnn <yessinerekik31@gmail.com>
2026-01-06 18:43:44 +0100

    Update LRU_Cache.h

--- src/LRU_Cache.h
+++ src/LRU_Cache.h
@@ -1,1 +1,25 @@
+#ifndef LRU_CACHE_H
+#define LRU_CACHE_H
+
+typedef struct Node{
+    int key;
+    int value;
+    struct Node* prev;
+    struct Node* next;
+} Node;
+
+typedef struct {
+    int capacity;
+    int size;
+    Node* head;
+    Node* tail;
+    Node** map;
+} LRUCache;
+
+LRUCache* lRUCacheCreate(int capacity);
+int lRUCacheGet(LRUCache* cache, int key);
+void lRUCachePut(LRUCache* cache, int key, int value);
+void lRUCacheFree(LRUCache* cache);
+
+#endif
 
